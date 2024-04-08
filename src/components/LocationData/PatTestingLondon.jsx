e0b2733e86da9e54a635fa840845aa078ef68b89
Darren Berry <darren@digitalmiracle.co.uk>
2024-04-08 23:54:35 +0100

    PAT testing London pages

---
+++ src/components/LocationData/PatTestingLondon.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import londonPostcodes from './londonPostcodes';
+
+const PatTestingLondon = () => {
+  return (
+    <div className="pat-testing-london max-w-7xl mx-auto px-4 py-10 bg-gray-900">
+      <h2 className="text-3xl text-white font-bold mb-4">PAT Testing in London</h2>
+
+      <h3 className="text-2xl text-white font-bold mb-4">Postcodes</h3>
+      <div className="lg:grid lg:grid-cols-4 gap-2">
+        {londonPostcodes.map((town, index) => (
+          <div key={`towns-${index}`}>
+            {/* Update the href to include the town name */}
+            <a href={`/pat-testing-london/${town.toLowerCase().replace(/\s+/g, '-')}`} className="text-gray-900 hover:bg-green-800 hover:text-white block p-2 bg-white">
+            PAT Testing {town.charAt(0).toUpperCase() + town.slice(1)}
+</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PatTestingLondon;
+ 
