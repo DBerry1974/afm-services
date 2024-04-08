5fea40c11d5700eb3283c31cff275fe4d407e2d2
Darren Berry <darren@digitalmiracle.co.uk>
2024-04-08 22:58:00 +0100

    FM template page

---
+++ src/components/LocationData/londonPostcodes.js
@@ -0,0 +1,15 @@
+// Postcodes in London for facilities management targeting
+const londonPostcodes = [
+    'E14', // Canary Wharf
+    'EC1', 'EC2', 'EC3', 'EC4', // The City
+    'W1', 'WC1', 'WC2', // West End
+    'W2', 'W8', 'W11', // West London
+    'N1', 'N7', // North London
+    'SE1', 'SE11', // South East London
+    'SW1', 'SW3', 'SW7', // South West London
+    'E1', 'E2', 'E1W', // East London
+    'NW1', 'NW3', // North West London
+  ];
+  
+  export { londonPostcodes };
+  
